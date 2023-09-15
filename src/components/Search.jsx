3f4856cbc454896a109a7faba31a989a07115268
TManhique <tnmanhique@gmail.com>
2023-09-15 16:55:00 +0200

    Final-version

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -4,6 +4,7 @@ import styled from 'styled-components';
 import {FaSearch} from 'react-icons/fa';
 import {useNavigate} from 'react-router-dom'
 
+
 function Search() {
   
   const [input, setInput] = useState("");
--- src/pages/Cuisine.jsx
+++ src/pages/Cuisine.jsx
@@ -28,12 +28,19 @@ function Cuisine() {
   },[params.type]);  
 
   return (
-    <Grid>
+    <Grid
+      animate={{opacity: 1}}
+      initial={{opacity: 0}}
+      exit={{opacity: 0}}
+      transition={{duration: 0.5}} >
+      
       {cuisine.map((item) =>{
         return(
-          <Card>
+          <Card key={item.id}>
+              <Link to={"/recipe/" + item.id }>
               <img src={item.image} alt="" />
-              <h4>{item.title}</h4>    
+              <h4>{item.title}</h4>
+              </Link>    
           </Card>
         )
       })}
@@ -41,7 +48,7 @@ function Cuisine() {
   )
 }
 
-const Grid = styled.div`
+const Grid = styled(motion.div)`
   display: grid;
   grid-template-columns: repeat(auto-fit, minmax(20rem, 1fr));
   grid-gap: 2rem;
